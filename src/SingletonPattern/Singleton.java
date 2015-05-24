4e11c1cb6da11171ac5ba11703caaaef3aeea413
DoubleIA <857480298@qq.com>
2015-05-24 22:40:43 +0800

    Singleton Pattern

---
+++ src/SingletonPattern/Singleton.java
@@ -0,0 +1,18 @@
+package SingletonPattern;
+
+public class Singleton {
+	private static volatile Singleton uniqueInstance; 
+	
+	private Singleton() {}
+	
+	public static  Singleton getInstance() {
+		if (uniqueInstance == null) {
+			synchronized (Singleton.class) {
+				if (uniqueInstance == null)
+					uniqueInstance = new Singleton();
+			}
+		}
+		return uniqueInstance;
+	}
+	
+}
